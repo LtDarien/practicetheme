ceaf35f649b540a3a8e792abc24362c4e895c957
Steven S <cubsms@gmail.com>
2014-04-28 17:28:20 -0500

    Custom Column Functions

--- functions.php
+++ functions.php
@@ -85,9 +85,68 @@ function practicetheme_post_types() {
 		'capability_type' => 'post',
 		'supports' => $arg['supports'],
 		'rewrite' => array('slug' => $arg['slug']),
-		'menu_positon' => (20 + Counter),
+		'menu_positon' => (20 + $counter),
 		));
 	$counter++;
 	}
 }
 add_action('init', 'practicetheme_post_types');
+
+function practicetheme_updated_messages($messages) {
+	global $post, $post_ID;
+
+	$types = array(
+		'pt_staff' => 'Person',
+		'pt_menu' => 'Item',
+		);
+
+	foreach($types as $type => $title) {
+		$messages[$type] = array(
+			0 => 1,
+			1 => sprintf(__('%s updated. <a href="%s">View %s</a>'),$title, esc_url(get_permalink( $post_ID)), $title),
+			2 => __('Custom field updated.'),
+			3 => __('Custom field deleted.'),
+			4 => __(strtolower($title).' updated.'),
+			5 => isset($_GET['revision']) ? sprintf(__('%s restored to previous version from %s'), $title, 
+				wp_post_revision_title( (int) $_GET['revision'], false)):false,
+			6 => sprintf(__('%s published. <a href="%s">View %s</a>'), $title, esc_url( get_permalink($post_ID )), strtolower($title)),
+			7 => __($title.' saved'),
+			8 => sprintf(__('%s submitted. <a target="_blank" href="%s">Preview %s</a>'),$title, 
+				esc_url(add_query_arg('preview','true',get_permalink($post_ID))), strtolower($title)),
+			9 => sprintf(__('%s scheduled for: <strong>%2$s</strong>. <a target="_blank" href="%3$s">Preview %1$s</a>'), $title,
+				date_i18n( __( 'M j, Y @ G:i' ), strtotime( $post -> post_date)), esc_url(get_permalink($post_ID))),
+			10 => sprintf(__('%s draft updated. <a target="_blank" href="%s">Preview %s</a>'), $title, 
+				esc_url(add_query_arg('preview', 'true', get_permalink($post_ID))), strtolower($title)),
+			);
+	}
+	return $messages;
+}
+add_filter('post_updated_messages', 'practicetheme_updated_messages');
+
+function practicetheme_custom_columns($cols) {
+	$cols = array(
+		'cb' => '<input type="checkbox"/>',
+		'title' => __('Title', 'practicetheme'),
+		'photo' => __('Thumbnail', 'practicetheme'),
+		'date' => __('Date', 'practicetheme'),
+	 );
+}
+add_filter("manage_pt_staff_posts_columns", "practicetheme_custom_columns");
+add_filter("manage_pt_meun_posts_columns", "practicetheme_custom_columns");
+
+function practicetheme_custome_column_contenet( $column, $post_id) {
+	switch ($column) {
+		case 'photo':
+			if(has_post_thumbnail($post_id)) {
+				echo get_the_post_thumbnail($post_id, array(50,50));
+			}
+			break;
+	}
+}
+add_action( "manage_pt_staff_posts_custom_column", "practicetheme_custome_column_contenet", 10, 2 );
+add_action( "manage_pt_menu_posts_custom_column", "practicetheme_custom_column_content", 10, 2 );
+
+function my_rewrite_flush() {
+	flush_rewrite_rules( );
+}
+add_action( 'after_switch_theme_download', 'practice_theme_flush_rewrite_rules' );
